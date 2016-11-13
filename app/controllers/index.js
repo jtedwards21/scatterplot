310d29a3345a259e787c40037e53d05680d4f2c7
James Ahlm <14782828428@qq.com>
2016-11-13 16:52:49 +0800

    Changed Organization to Include Parent Element

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,22 +7,36 @@ d3.json(addr, function(data){
 var maxRank = d3.max(data, function(d){return d.Place})
 var maxTime = d3.max(data, function(d){return d.Seconds})
 var minTime = d3.min(data, function(d){return d.Seconds})
-var xScale = d3.scaleLinear().domain([minTime, maxTime]).range([0, 300])
-var yScale = d3.scaleLinear().domain([0, maxRank]).range([0, height])
+var xScale = d3.scaleLinear().domain([maxTime, minTime]).range([0,300])
+var yScale = d3.scaleLinear().domain([maxRank, 0]).range([height, 0])
 var radius = 1;
 
-var yAxis = d3.axisRight().scale(yScale)
-//var yAxis = d3.svg.axis().scale(yScale.orient("right"));
-d3.select("svg").append("g").attr("id", "yAxisG").call(yAxis);
+var yAxis = d3.axisLeft().scale(yScale).tickSize(0)
+d3.select("svg").append("g").attr("id", "yAxisG").attr("transform", "translate(0,0)").call(yAxis);//These transforms should be factor of the size
 
-var xAxis = d3.axisBottom().scale(xScale);
+var xAxis = d3.axisBottom().scale(xScale).tickSize(0)
 d3.select("svg").append("g").attr("id", "xAxisG").attr("transform", "translate(0," + height + ")").call(xAxis);
 
+//Append Parent Element
+d3.select("svg")
+.append("g")
+.attr("id", "pointsG")
+.attr("transform", "translate(0,0)")
+.selectAll("g")
+.data(data)
+.enter()
+.append("g")
+.attr("class","overallG")
 
-//Order Data by Place
-data.sort(function(a, b){
-  return a.Place - b.Place
-})
+var pointsG = d3.selectAll("g.overallG")
+
+pointsG
+.append("circle")
+.attr("r", radius)
+.attr("cx", function(d, i){return xScale(d.Seconds)})
+.attr("cy", function(d) {return yScale(d.Place)})
+.style("fill", "black")
+.style("stroke-width", "1px")
 
 //Draw some circles
 d3.select("svg")
@@ -31,8 +45,10 @@ d3.select("svg")
 .enter()
 .append("circle")
 .attr("r", radius)
-.attr("cx", function(d, i){return xScale(d.Seconds)})
-.attr("cy", function(d) {return height - yScale(d.Place)})
+
+//Add the hover attributes
+//Add Written Names
+
 
 
 })
