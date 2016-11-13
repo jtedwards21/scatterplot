2e1edfd2fd26522c6cf7c410e72d39f64cf04238
James Ahlm <14782828428@qq.com>
2016-11-13 15:17:41 +0800

    Fixed Errors, Can Disply Plot

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,4 @@
-var addr = https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json
+var addr = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
 var height = 100;
 var width = 100;
 
@@ -6,15 +6,16 @@ d3.json(addr, function(data){
 
 var maxRank = d3.max(data, function(d){return d.Place})
 var maxTime = d3.max(data, function(d){return d.Seconds})
-var xScale = d3.scaleLinear().domain([0, maxTime]).range([0, width])
+var minTime = d3.min(data, function(d){return d.Seconds})
+var xScale = d3.scaleLinear().domain([minTime, maxTime]).range([0, width])
 var yScale = d3.scaleLinear().domain([0, maxRank]).range([0, height])
-var radius = 5;
+var radius = 1;
 
 
 //Order Data by Place
 data.sort(function(a, b){
   return a.Place - b.Place
-}
+})
 
 //Draw some circles
 d3.select("svg")
@@ -24,7 +25,7 @@ d3.select("svg")
 .append("circle")
 .attr("r", radius)
 .attr("cx", function(d, i){return xScale(d.Seconds)})
-.attr.("cy", function(d) {return height - yScale(d.Place)})
+.attr("cy", function(d) {return height - yScale(d.Place)})
 
 
 })
