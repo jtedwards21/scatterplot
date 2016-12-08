be8fcac269c51f05ec7a5b347a360a058c3759b1
James Ahlm <14782828428@qq.com>
2016-12-08 17:08:13 +0800

    Fixed Port

--- server.js
+++ server.js
@@ -6,6 +6,4 @@ app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
 routes(app)
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(process.env.PORT || 5000);
