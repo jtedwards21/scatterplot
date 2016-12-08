272f86e77f90613464fe610c6c4002596e54f435
James Ahlm <14782828428@qq.com>
2016-12-08 16:27:29 +0800

    Styled

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,9 +3,9 @@ var addr = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/
 
 var margins = {
 "top": 50,
-"left": 30,
-"bottom": 20,
-"right": 10
+"left": 60,
+"bottom": 50,
+"right": 30
 }
 
 var height = 700 - margins.top - margins.bottom;
@@ -39,10 +39,21 @@ d3.select("svg")
 .attr("transform", "translate(" + margins.left + "," + margins.top + ")")
 
 var yAxis = d3.axisLeft().scale(yScale).tickSize(0)
-d3.select("#containerG").append("g").attr("id", "yAxisG").attr("transform", "translate(0,0)").call(yAxis);//These transforms should be factor of the size
+d3.select("#containerG").append("g").attr("id", "yAxisG").attr("transform", "translate(-20,0)").call(yAxis)
+
+//Append Label?
+.append("text")
+  .attr("x", 0)
+  .attr("y", -35)
+  .attr("dy", ".35em")
+  .style("text-anchor", "end")
+  .attr("transform", "rotate(-90)")
+  .text("Ranking");
 
 var xAxis = d3.axisBottom().scale(xScale).tickSize(0)
-d3.select("#containerG").append("g").attr("id", "xAxisG").attr("transform", "translate(0," + height + ")").call(xAxis);
+d3.select("#containerG").append("g").attr("id", "xAxisG").attr("transform", "translate(0," + (height + 20) + ")").call(xAxis);
+
+
 
 //Create a container for the points
 
@@ -66,13 +77,33 @@ var div= d3.select("#box").append("div")
 var pointsG = d3.selectAll("g.overallG")
 
 pointsG
+.append("text")
+.text(function(d){
+  return d.Name;
+})
+.attr("x", function(d){return xScale(d.Seconds) + 10})
+.attr("y", function(d) {return yScale(d.Place) + 5})
+
+pointsG
 .append("circle")
 .attr("r", radius)
 .attr("cx", function(d, i){return xScale(d.Seconds)})
 .attr("cy", function(d) {return yScale(d.Place)})
-.style("fill", "black")
+.style("fill", function(d){
+  if(d.Doping == ""){
+    return "#01CFAF";
+  } else {
+    return "#DC5038";
+  }
+})
 .style("stroke-width", "3px")
-.style("stroke", "#7035BB")
+.style("stroke", function(d){
+  if(d.Doping == ""){
+    return "#01CFAF";
+  } else {
+    return "#DC5038";
+  }
+})
 //Code for tooltip
 .on("mouseover", function(d){
 var rect = d3.select(this);
@@ -85,11 +116,19 @@ var url = d.URL;
 var place = d.Place;
 var raceTime = d.Time;
 div.style("opacity", .9)
-div.html("<span class='name'>" + name + "</span><br><span class='year'>" + year + "</span><br><span class='nationality'>" + nationality + "</span><br><span class='doping'>" + doping + "</span><br><span class='url'>" + url + "</span>")
+div.html("<span class='name'>" + name + "</span><br><span class='year'>" + year + "</span><br><span class='nationality'>" + nationality + "</span><br><a href='"+ url + "'>" + doping + "</a>")
 .style("left", (d3.event.pageX + 5) + "px")
 .style("top", (d3.event.pageY - 50) + "px");
 
 })
 
+//Turn off the tooltip
+
+.on("mouseleave", function(d){
+var rect = d3.select(this);
+rect.attr("class", "mouseleave");
+div.style("opacity", 0);
+
+})
 
 })
